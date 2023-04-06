abbb4c0725435d3534f7d109f3ea10e0ff10ad79
Laura Reifsnyder <laura.reifsnyder@kellogg.northwestern.edu>
2023-04-06 02:22:12 +0000

    COMMIT

--- lab-3-3.sql
+++ lab-3-3.sql
@@ -15,9 +15,9 @@
 -- | 2019 | Houston Astros                | 107       |
 -- | 2004 | St. Louis Cardinals           | 105       |
 
-SELECT year, name, wins
+SELECT year, name, MAX(wins)
 FROM teams
 WHERE year >= 1960
 GROUP BY year
-ORDER BY wins DESC
+ORDER BY MAX(wins) DESC
 ;
